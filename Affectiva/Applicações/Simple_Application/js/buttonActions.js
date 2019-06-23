bef00d11040cc8d26ca4e61e25c6b1715e2027f2
Eduardo Pinheiro <tm.twomaker@gmail.com>
2019-06-23 01:22:54 -0300

    reset state quando reset

--- Affectiva/Applicações/Simple_Application/js/buttonActions.js
+++ Affectiva/Applicações/Simple_Application/js/buttonActions.js
@@ -5,6 +5,23 @@ function init() {
 
 function reset() {
   detector.stop()
+  
+  const resetState = {
+    emotions: {
+      anger: [],
+      contempt: [],
+      disgust: [],
+      engagement: [],
+      fear: [],
+      joy: [],
+      sadness: [],
+      valence: [],
+    },
+    frameFlag: false
+  }
+
+  state.setState(resetState)
+
   render('start')
 }
 
